feb6b52f44df2fa6df424be9c825a2cce8b7037b
TheJudyEssam <ju121.farouk@gmail.com>
2025-07-24 09:56:00 +0300

    xP

--- app/src/main/java/com/example/nectar/ui/screens/ProductDetailsScreen/ProductDetails.kt
+++ app/src/main/java/com/example/nectar/ui/screens/ProductDetailsScreen/ProductDetails.kt
@@ -1,6 +1,8 @@
 package com.example.nectar.ui.screens.ProductDetailsScreen
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,12 +10,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -47,6 +54,10 @@ fun ProductBody(
     productNutrition: Map<String, String> = emptyMap(),
     productPrice: Float = 0.0f
 ){
+
+    var details_visible by remember { mutableStateOf(false) }
+
+
     LazyColumn {
         item{
             Image(
@@ -91,6 +102,46 @@ fun ProductBody(
                 }
 
             }
+        }
+
+        item{
+
+            Column(
+                modifier = modifier.padding(16.dp)
+            ){
+
+                Row(){
+                    Text(
+                        "Product Details",
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = modifier.weight(1f)
+                    )
+
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down",
+                        modifier = modifier.clickable(
+                            enabled = true, onClick = {
+                                details_visible = !details_visible
+                                
+                            }
+                        )
+                    )
+                }
+
+                AnimatedVisibility(
+                    details_visible == true
+                ) {
+                    Text(
+                        productDescription,
+                        style = MaterialTheme.typography.labelSmall,
+                        textAlign = TextAlign.Left
+                    )
+                }
+
+            }
+
+
         }
 
         item{
---
+++ app/src/main/java/com/example/nectar/ui/screens/ProductDetailsScreen/Sections/ProductNameSec.kt
@@ -0,0 +1,2 @@
+package com.example.nectar.ui.screens.ProductDetailsScreen.Sections
+
